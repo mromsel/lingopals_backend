ad113045eeae7c79f16b2b5af92cd1ab030ef33d
mromsel <miriamromero2@gmail.com>
2024-02-07 17:07:36 +0100

    Initial Spring Boot project

---
+++ lingopals_backend/src/main/java/com/romsel/lingopal_backend/LingopalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.romsel.lingopal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingopalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingopalBackendApplication.class, args);
+	}
+
+}
---
+++ lingopals_backend/src/test/java/com/romsel/lingopal_backend/LingopalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romsel.lingopal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingopalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
