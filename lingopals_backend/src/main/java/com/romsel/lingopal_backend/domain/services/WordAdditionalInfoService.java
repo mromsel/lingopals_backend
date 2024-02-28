12cbd332515b170a5d7259143e622237f7cd2322
mromsel <miriamromero2@gmail.com>
2024-02-28 21:51:18 +0100

    Add WordAdditionalInfoController basic method

--- lingopals_backend/src/main/java/com/romsel/lingopal_backend/domain/services/WordAdditionalInfoService.java
+++ lingopals_backend/src/main/java/com/romsel/lingopal_backend/domain/services/WordAdditionalInfoService.java
@@ -1,8 +1,15 @@
 package com.romsel.lingopal_backend.domain.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import com.romsel.lingopal_backend.application.exceptions.ExceptionMessages;
+import com.romsel.lingopal_backend.application.exceptions.WordAdditionalInfoException;
+import com.romsel.lingopal_backend.domain.entities.languages_content.Word;
+import com.romsel.lingopal_backend.domain.entities.languages_content.WordAdditionalInfo;
 import com.romsel.lingopal_backend.domain.repositories.WordAdditionalInfoRepository;
 
 @Service
@@ -10,5 +17,9 @@ public class WordAdditionalInfoService {
 
     @Autowired
     private WordAdditionalInfoRepository wordAdditionalInfoRepository;
+
+    public WordAdditionalInfo getWordAdditionalInfoByWord(Word word) {
+        return wordAdditionalInfoRepository.findByWord(word);
+    }
 
 }
