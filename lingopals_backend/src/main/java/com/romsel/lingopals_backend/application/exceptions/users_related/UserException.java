ca19a8e55ed4bd09b1c74487d1a1e6f0974fee97
mromsel <miriamromero2@gmail.com>
2024-03-26 01:16:16 +0100

    Add exceptions for User, UserActivity, UserCompletedLessons and UserProgressData

---
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/application/exceptions/users_related/UserException.java
@@ -0,0 +1,22 @@
+package com.romsel.lingopals_backend.application.exceptions.users_related;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public class UserException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    private final HttpStatus httpStatus;
+    private final List<String> customErrors;
+
+    public UserException(HttpStatus httpStatus, List<String> customErrors) {
+        this.httpStatus = httpStatus;
+        this.customErrors = customErrors;
+    }
+
+}
