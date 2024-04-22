66d5e34ff0beadb5cdfbe6faa7a708e0055c8fe1
mromsel <miriamromero2@gmail.com>
2024-04-22 18:14:27 +0200

    Upgrade LessonController and response with lists of words

--- lingopals_backend/src/main/java/com/romsel/lingopals_backend/application/rest/words_related/LessonController.java
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/application/rest/words_related/LessonController.java
@@ -12,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.romsel.lingopals_backend.application.exceptions.words_related.LessonException;
 import com.romsel.lingopals_backend.application.response.words_related.LessonDto;
+import com.romsel.lingopals_backend.application.response.words_related.WordDto;
 import com.romsel.lingopals_backend.domain.entities.words_related.Lesson;
+import com.romsel.lingopals_backend.domain.entities.words_related.WordReference;
 import com.romsel.lingopals_backend.domain.services.words_related.LessonService;
+import com.romsel.lingopals_backend.domain.services.words_related.WordService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +30,9 @@ public class LessonController {
 
     @Autowired
     private LessonService lessonService;
+
+    @Autowired
+    private WordService wordService;
 
     @GetMapping("/lessons")
     public List<LessonDto> getAllLessons() {
@@ -48,6 +54,16 @@ public class LessonController {
 
         LessonDto lessonDto = modelMapper.map(lesson, LessonDto.class);
 
+        List<Long> idsWordReferences = lesson.getListWordsReferences().stream().map(WordReference::getIdWordRef)
+                .toList();
+        List<WordDto> wordsOrigin = wordService.getWordsByWordReferencesAndLanguage(idsWordReferences, 1).stream()
+                .map(word -> modelMapper.map(word, WordDto.class)).toList();
+
+        List<WordDto> wordsDestiny = wordService.getWordsByWordReferencesAndLanguage(idsWordReferences, 2).stream()
+                .map(word -> modelMapper.map(word, WordDto.class)).toList();
+
+        lessonDto.setListWordsOrigin(wordsOrigin);
+        lessonDto.setListWordDestiny(wordsDestiny);
         return new ResponseEntity<>(lessonDto, HttpStatus.OK);
     }
 
