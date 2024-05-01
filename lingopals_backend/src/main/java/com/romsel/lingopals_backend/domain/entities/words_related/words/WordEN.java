4c95b0fe7bd8f23f32bfe6df9f6eb6737ff94974
mromsel <miriamromero2@gmail.com>
2024-05-01 13:19:53 +0200

    Add new Word mappedsuperclass and specifics

---
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/domain/entities/words_related/words/WordEN.java
@@ -0,0 +1,10 @@
+package com.romsel.lingopals_backend.domain.entities.words_related.words;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "words_en")
+public class WordEN extends Word {
+
+}
---
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/domain/entities/words_related/words/WordKO.java
@@ -0,0 +1,10 @@
+package com.romsel.lingopals_backend.domain.entities.words_related.words;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "words_ko")
+public class WordKO extends Word {
+
+}
