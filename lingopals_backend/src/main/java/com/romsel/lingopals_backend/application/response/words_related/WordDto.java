9323678d8d050a7f3fd7e40511398154d0e840fa
mromsel <miriamromero2@gmail.com>
2024-05-01 13:30:48 +0200

    Update WordDto and create WordFullDto

--- lingopals_backend/src/main/java/com/romsel/lingopals_backend/application/response/words_related/WordDto.java
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/application/response/words_related/WordDto.java
@@ -3,7 +3,7 @@ package com.romsel.lingopals_backend.application.response.words_related;
 import lombok.Data;
 
 /**
- * @see com.romsel.lingopals_backend.domain.entities.words_related.Word
+ * @see com.romsel.lingopals_backend.domain.entities.words_related.words.Word
  */
 @Data
 public class WordDto implements Comparable<WordDto> {
@@ -11,13 +11,12 @@ public class WordDto implements Comparable<WordDto> {
     private Long idWord;
     private Long idWordRef;
     private Integer idLanguage;
-    private String word;
+    private String wordString;
     private String definition;
-    private WordAdditionalInfoDto additionalInfo;
 
     @Override
     public int compareTo(WordDto o) {
-        return this.word.compareTo(o.word);
+        return this.wordString.compareTo(o.wordString);
     }
 
 }
