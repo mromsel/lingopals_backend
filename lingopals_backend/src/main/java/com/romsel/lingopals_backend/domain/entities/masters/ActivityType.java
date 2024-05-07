7780da4b9afe9dd12db13ac477beb9bb669e421d
mromsel <miriamromero2@gmail.com>
2024-05-07 12:14:46 +0200

    Add ActivityType entity

---
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/domain/entities/masters/ActivityType.java
@@ -0,0 +1,27 @@
+package com.romsel.lingopals_backend.domain.entities.masters;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "activity_types")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_activity_type")
+    private Integer idActivityType;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+}
