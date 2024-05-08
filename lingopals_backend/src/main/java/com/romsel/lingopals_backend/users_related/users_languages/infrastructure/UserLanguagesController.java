a585948b616d8e9bbd32cd7ef5ad399f13ce73bb
mromsel <miriamromero2@gmail.com>
2024-05-08 21:11:58 +0200

    Add methods in controller to get all user languages and to change the preferred one

--- lingopals_backend/src/main/java/com/romsel/lingopals_backend/users_related/users_languages/infrastructure/UserLanguagesController.java
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/users_related/users_languages/infrastructure/UserLanguagesController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api")
@@ -19,6 +23,14 @@ public class UserLanguagesController {
     @Autowired
     private UserLanguagesService userLanguagesService;
 
+    @GetMapping("/users-languages")
+    public List<UserLanguagesDto> getAllUserLanguages() {
+        return userLanguagesService.getAllUserLanguages()
+                .stream()
+                .map(userLanguage -> modelMapper.map(userLanguage, UserLanguagesDto.class))
+                .toList();
+    }
+
     @GetMapping("/users-languages/{idUser}")
     public List<UserLanguagesDto> getUserLanguages(@PathVariable Long idUser) {
         return userLanguagesService.getUserLanguagesByUserID(idUser)
@@ -26,4 +38,14 @@ public class UserLanguagesController {
                 .map(userLanguage -> modelMapper.map(userLanguage, UserLanguagesDto.class))
                 .toList();
     }
+
+    @PostMapping("/users-language/change-preferred")
+    public ResponseEntity<?> changePreferredUserLanguages(@RequestBody UserLanguagesDto userLanguagesDto) {
+
+        this.userLanguagesService.changePreferredUserLanguages(userLanguagesDto.getUser().getIdUser(),
+                userLanguagesDto.getIdUserLanguages());
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
