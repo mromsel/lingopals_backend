88025fcf7ccb16e633a524412e1a1e32bee8e677
mromsel <miriamromero2@gmail.com>
2024-05-11 23:18:04 +0200

    Create UserLevelManager with logic to addXp

---
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/users_related/users_progress/domain/level_up/UserLevelManager.java
@@ -0,0 +1,47 @@
+package com.romsel.lingopals_backend.users_related.users_progress.domain.level_up;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.romsel.lingopals_backend.masters.xp_levels.domain.XPLevel;
+import com.romsel.lingopals_backend.masters.xp_levels.infrastructure.XPLevelService;
+import com.romsel.lingopals_backend.users_related.users_progress.domain.UserProgressData;
+import com.romsel.lingopals_backend.users_related.users_progress.infrastructure.UserProgressDataService;
+import com.romsel.lingopals_backend.users_related.users_progress.infrastructure.level_up.UserLevelUpdateDto;
+
+@Service
+public class UserLevelManager {
+
+    @Autowired
+    private UserProgressDataService userProgressDataService;
+
+    @Autowired
+    private XPLevelService xpLevelService;
+
+    public UserLevelUpdateDto addXP(Long idUser, Integer xpGained) {
+        UserProgressData userProgress = userProgressDataService.getUserProgressByUserID(idUser);
+
+        Long initialXpPoints = userProgress.getXpPoints();
+
+        Long currentXpPoints = initialXpPoints + xpGained;
+
+        userProgress.setXpPoints(currentXpPoints);
+
+        Boolean isLevelUp = false;
+        XPLevel newLevel = null;
+
+        if (currentXpPoints > userProgress.getXpLevel().getXpRangeEnd()) {
+            System.out.println("Level up");
+            isLevelUp = true;
+            newLevel = xpLevelService.findByLevel(userProgress.getXpLevel().getLevelNumber() + 1);
+            userProgress.setXpLevel(newLevel);
+        }
+
+        // TODO: check for medal
+
+        userProgressDataService.save(userProgress);
+
+        UserLevelUpdateDto userLevelUpdateDto = new UserLevelUpdateDto(initialXpPoints, xpGained, isLevelUp, newLevel);
+        return userLevelUpdateDto;
+    }
+}
