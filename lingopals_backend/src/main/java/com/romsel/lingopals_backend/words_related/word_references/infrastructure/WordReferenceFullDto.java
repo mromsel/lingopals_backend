31ef7396944d1e3406edb0b241907d0a77f8e059
mromsel <miriamromero2@gmail.com>
2024-05-12 21:06:50 +0200

    Create WordReferenceFullDto

---
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/words_related/word_references/infrastructure/WordReferenceFullDto.java
@@ -0,0 +1,19 @@
+package com.romsel.lingopals_backend.words_related.word_references.infrastructure;
+
+import com.romsel.lingopals_backend.masters.language_levels.infrastructure.LanguageLevelDto;
+import com.romsel.lingopals_backend.words_related.categories.infrastructure.CategoryDto;
+import lombok.Data;
+
+/**
+ * @see com.romsel.lingopals_backend.words_related.word_references.domain.WordReference
+ */
+@Data
+public class WordReferenceFullDto {
+
+    private Long idWordRef;
+    private String englishWord;
+    private String imageUrl;
+    private LanguageLevelDto languageLevel;
+    private CategoryDto category;
+
+}
