642fbc67140cee961e7c621fea7f9be00f853325
mromsel <miriamromero2@gmail.com>
2024-05-16 19:17:35 +0200

    Create WordsRelatedController and change endpoints with prefix "admin-panel"

--- lingopals_backend/src/main/java/com/romsel/lingopals_backend/admin_panel/infrastructure/words_related/WordsRelatedController.java
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/admin_panel/infrastructure/words_related/WordsRelatedController.java
@@ -1,4 +1,4 @@
-package com.romsel.lingopals_backend.admin_panel.infrastructure.masters;
+package com.romsel.lingopals_backend.admin_panel.infrastructure.words_related;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,17 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api")
-public class MastersController {
+public class WordsRelatedController {
 
     @Autowired
     private ModelMapper modelMapper;
 
     @Autowired
-    private MastersService mastersService;
+    private WordsRelatedService wordsRelatedService;
 
-    @GetMapping("/admin-panel/masters")
-    public MastersDto getAllMasters() {
-        return modelMapper.map(mastersService.getAllMasters(), MastersDto.class);
+    @GetMapping("/admin-panel/words-related")
+    public WordsRelatedDto getWordsRelated() {
+        return modelMapper.map(wordsRelatedService.getWordsRelated(), WordsRelatedDto.class);
     }
 
 }
