e2ab0102551915a92d3cde34895f946d5ccdb100
mromsel <miriamromero2@gmail.com>
2024-05-30 18:48:19 +0200

    Add ActivityType names in Constants class

--- lingopals_backend/src/main/java/com/romsel/lingopals_backend/common/Constants.java
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/common/Constants.java
@@ -3,5 +3,12 @@ package com.romsel.lingopals_backend.common;
 /** This class contains constants that are shared by diferent classes */
 public class Constants {
 
-    public static int XP_GAINED_PER_ACTIVITY = 50;
+    private Constants() {
+    }
+
+    public static final int XP_GAINED_PER_ACTIVITY = 50;
+
+    // ACTIVITY TYPES
+    public static final String ACTIVITY_TYPE_LESSON = "LESSON";
+    public static final String ACTIVITY_TYPE_REVIEW = "REVIEW";
 }
