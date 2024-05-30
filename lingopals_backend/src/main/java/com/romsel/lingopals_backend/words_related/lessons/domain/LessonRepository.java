a3808161324f7dc8e07a7c32e461d40e39d5df93
mromsel <miriamromero2@gmail.com>
2024-05-30 19:06:08 +0200

    Add method findByLanguageLevel to LessonRepository

--- lingopals_backend/src/main/java/com/romsel/lingopals_backend/words_related/lessons/domain/LessonRepository.java
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/words_related/lessons/domain/LessonRepository.java
@@ -2,8 +2,11 @@ package com.romsel.lingopals_backend.words_related.lessons.domain;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import com.romsel.lingopals_backend.masters.language_levels.domain.LanguageLevel;
 
 @Repository
 public interface LessonRepository extends CrudRepository<Lesson, Integer> {
 
+    List<Lesson> findByLanguageLevel(LanguageLevel languageLevel);
 }
