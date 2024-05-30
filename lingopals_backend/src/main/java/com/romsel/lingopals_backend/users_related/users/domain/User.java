2937e9ce27a617d99241db6d87e741011ca430fb
mromsel <miriamromero2@gmail.com>
2024-05-30 18:55:30 +0200

    Remove User from UserActivity

--- lingopals_backend/src/main/java/com/romsel/lingopals_backend/users_related/users/domain/User.java
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/users_related/users/domain/User.java
@@ -8,7 +8,6 @@ import java.util.Set;
 
 import com.romsel.lingopals_backend.masters.languages.domain.Language;
 import com.romsel.lingopals_backend.masters.profiles.domain.Profile;
-import com.romsel.lingopals_backend.users_related.users_activity.domain.UserActivity;
 import com.romsel.lingopals_backend.users_related.users_languages.domain.UserLanguages;
 import com.romsel.lingopals_backend.users_related.users_progress.domain.UserProgressData;
 import com.romsel.lingopals_backend.users_related.users_relationships.domain.UserRelationships;
@@ -18,7 +17,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -76,9 +74,6 @@ public class User {
     @OneToOne(orphanRemoval = true, cascade = CascadeType.ALL)
     @JoinColumn(name = "id_user_progress_data", nullable = true)
     private UserProgressData userProgressData;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
-    private List<UserActivity> activities;
 
     @JoinTable(name = "users_profiles", joinColumns = @JoinColumn(name = "id_user"), inverseJoinColumns = @JoinColumn(name = "id_profile"))
     @ManyToMany
--- lingopals_backend/src/main/java/com/romsel/lingopals_backend/users_related/users_activity/domain/UserActivityRepository.java
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/users_related/users_activity/domain/UserActivityRepository.java
@@ -3,13 +3,13 @@ package com.romsel.lingopals_backend.users_related.users_activity.domain;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.romsel.lingopals_backend.users_related.users.domain.User;
+import com.romsel.lingopals_backend.users_related.users_languages.domain.UserLanguages;
 
 import java.util.List;
 
 @Repository
 public interface UserActivityRepository extends CrudRepository<UserActivity, Long> {
 
-    List<UserActivity> findByUser(User user);
+    List<UserActivity> findByUserLanguages(UserLanguages userLanguages);
 
 }
