d4bb9ec5073fdf92d73db1746e3f82a5ea214fdd
mromsel <miriamromero2@gmail.com>
2024-05-30 18:57:43 +0200

    Create LessonDisplay classes and logic

---
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/words_related/lessons/infrastructure/display/LessonDisplayDto.java
@@ -0,0 +1,18 @@
+package com.romsel.lingopals_backend.words_related.lessons.infrastructure.display;
+
+import com.romsel.lingopals_backend.masters.activity_types.infrastructure.ActivityTypeDto;
+import com.romsel.lingopals_backend.masters.language_levels.infrastructure.LanguageLevelDto;
+import com.romsel.lingopals_backend.users_related.users_completed_lessons.infrastructure.UserCompletedLessonsDto;
+
+import lombok.Data;
+
+@Data
+public class LessonDisplayDto {
+
+    private Integer idLesson;
+    private String lessonName;
+    private LanguageLevelDto languageLevel;
+    private ActivityTypeDto activityType;
+    private Boolean isCompleted;
+    private UserCompletedLessonsDto userCompletedLesson;
+}
