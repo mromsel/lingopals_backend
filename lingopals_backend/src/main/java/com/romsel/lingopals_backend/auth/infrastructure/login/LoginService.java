dc5c8cb247c5e7082cbc464452fec9b5a38bac62
mromsel <miriamromero2@gmail.com>
2024-06-07 17:23:11 +0200

    Fix login service method calling from user service

--- lingopals_backend/src/main/java/com/romsel/lingopals_backend/auth/infrastructure/login/LoginService.java
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/auth/infrastructure/login/LoginService.java
@@ -14,7 +14,6 @@ import com.romsel.lingopals_backend.common.exceptions.ExceptionMessages;
 import com.romsel.lingopals_backend.configuration.jwt.JwtService;
 import com.romsel.lingopals_backend.users_related.users.domain.UserException;
 import com.romsel.lingopals_backend.users_related.users.domain.UserRepository;
-
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -28,11 +27,10 @@ public class LoginService {
         public AuthResponse login(LoginRequest request) {
                 authenticationManager.authenticate(
                                 new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
-                UserDetails user = userRepository.findByUsernameOrEmail(request.getUsername())
+                UserDetails user = userRepository.findByUsername(request.getUsername())
                                 .orElseThrow(() -> new UserException(HttpStatus.NOT_FOUND,
                                                 List.of(ExceptionMessages.USER_NOT_FOUND)));
                 String token = jwtService.getToken(user);
-
                 return AuthResponse.builder()
                                 .token(token)
                                 .build();
