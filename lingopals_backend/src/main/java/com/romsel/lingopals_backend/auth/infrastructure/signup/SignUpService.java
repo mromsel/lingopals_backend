52c90b7640eaf6374ca84ae6898ab61fe22a0c6d
mromsel <miriamromero2@gmail.com>
2024-06-07 17:21:12 +0200

    Modify sign up method to create user progress data

--- lingopals_backend/src/main/java/com/romsel/lingopals_backend/auth/infrastructure/signup/SignUpService.java
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/auth/infrastructure/signup/SignUpService.java
@@ -9,43 +9,66 @@ import org.springframework.stereotype.Service;
 
 import com.romsel.lingopals_backend.auth.application.AuthResponse;
 import com.romsel.lingopals_backend.auth.application.signup.SignUpRequest;
+import com.romsel.lingopals_backend.common.Constants;
 import com.romsel.lingopals_backend.configuration.jwt.JwtService;
 import com.romsel.lingopals_backend.masters.languages.infrastructure.LanguageService;
+import com.romsel.lingopals_backend.masters.profiles.infrastructure.ProfileService;
+import com.romsel.lingopals_backend.masters.xp_levels.infrastructure.XPLevelService;
 import com.romsel.lingopals_backend.users_related.users.domain.User;
 import com.romsel.lingopals_backend.users_related.users.infrastructure.UserService;
+import com.romsel.lingopals_backend.users_related.users_progress.domain.UserProgressData;
+import com.romsel.lingopals_backend.users_related.users_progress.infrastructure.UserProgressDataService;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class SignUpService {
 
-    private final UserService userService;
+        private final UserService userService;
 
-    private final LanguageService languageService;
+        private final UserProgressDataService userProgressDataService;
 
-    private final JwtService jwtService;
+        private final LanguageService languageService;
 
-    private final PasswordEncoder passwordEncoder;
+        private final ProfileService profileService;
 
-    public AuthResponse signUp(SignUpRequest request) {
-        User user = User.builder()
-                .username(request.getUsername())
-                .email(request.getEmail())
-                .passwordHash(passwordEncoder.encode(request.getPassword()))
-                .preferredLanguage(languageService.getLanguageByID(request.getIdPreferredLanguage()))
-                .timeZone(request.getTimeZone())
-                .profile(null) // TODO: ADD PROFILE
-                .registrationDate(ZonedDateTime.of(LocalDateTime.now(), ZoneId.of(request.getTimeZone())))
-                .build();
+        private final XPLevelService xpLevelService;
 
-        User createdUser = userService.save(user);
+        private final JwtService jwtService;
 
-        // TODO: CREATE PROGRESS DATA
+        private final PasswordEncoder passwordEncoder;
 
-        System.out.println(user);
-        return AuthResponse.builder()
-                .token(jwtService.getToken(user))
-                .build();
-    }
+        public AuthResponse signUp(SignUpRequest request) {
+                User user = User.builder()
+                                .username(request.getUsername())
+                                .email(request.getEmail())
+                                .passwordHash(passwordEncoder.encode(request.getPassword()))
+                                .preferredLanguage(languageService.getLanguageByID(request.getIdPreferredLanguage()))
+                                .timeZone(request.getTimeZone())
+                                .profile(profileService.findByName(Constants.PROFILE_USER_STANDARD))
+                                .registrationDate(
+                                                ZonedDateTime.of(LocalDateTime.now(), ZoneId.of(request.getTimeZone())))
+                                .build();
 
+                User createdUser = userService.save(user);
+
+                UserProgressData newUserProgressData = UserProgressData
+                                .builder()
+                                .idUserProgressData(createdUser.getIdUser())
+                                .user(createdUser)
+                                .coins(0)
+                                .currentStreak(0)
+                                .xpPoints(0L)
+                                .xpLevel(xpLevelService.findByLevel(1))
+                                .build();
+
+                UserProgressData progressData = userProgressDataService.save(newUserProgressData);
+                createdUser.setUserProgressData(progressData);
+                userService.save(user);
+
+                return AuthResponse.builder()
+                                .token(jwtService.getToken(user))
+                                .build();
+        }
 }
