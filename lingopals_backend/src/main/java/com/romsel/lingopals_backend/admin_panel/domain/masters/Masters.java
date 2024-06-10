76a7732ccbabfb278a0fe98fc1cab4d8cf7cf559
mromsel <miriamromero2@gmail.com>
2024-06-10 16:31:55 +0200

    Add semantic categories to masters

--- lingopals_backend/src/main/java/com/romsel/lingopals_backend/admin_panel/domain/masters/Masters.java
+++ lingopals_backend/src/main/java/com/romsel/lingopals_backend/admin_panel/domain/masters/Masters.java
@@ -9,6 +9,7 @@ import com.romsel.lingopals_backend.masters.languages.domain.Language;
 import com.romsel.lingopals_backend.masters.profiles.domain.Profile;
 import com.romsel.lingopals_backend.masters.writing_systems.domain.WritingSystem;
 import com.romsel.lingopals_backend.masters.xp_levels.domain.XPLevel;
+import com.romsel.lingopals_backend.words_related.semantic_categories.domain.SemanticCategory;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,6 +24,7 @@ public class Masters {
     private List<LanguageLevel> languageLevels;
     private List<Language> languages;
     private List<Profile> profiles;
+    private List<SemanticCategory> semanticCategories;
     private List<WritingSystem> writingSystems;
     private List<XPLevel> xpLevels;
 }
