41d06f0b0de82f03d3c9a5c1ddc0d97f796222a0
mromsel <miriamromero2@gmail.com>
2024-06-12 06:57:26 +0200

    Fix WordDto mapping in WordControllerImpl

--- src/main/java/com/romsel/lingopals_backend/words_related/words/infrastructure/WordControllerImpl.java
+++ src/main/java/com/romsel/lingopals_backend/words_related/words/infrastructure/WordControllerImpl.java
@@ -2,7 +2,6 @@ package com.romsel.lingopals_backend.words_related.words.infrastructure;
 
 import java.util.List;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +16,6 @@ import com.romsel.lingopals_backend.words_related.words.domain.Word;
 
 public abstract class WordControllerImpl<E extends Word, S extends WordServiceImpl<E, Long>>
         implements WordController<E, Long> {
-
-    @Autowired
-    private ModelMapper modelMapper;
 
     @Autowired
     private S service;
@@ -37,14 +33,14 @@ public abstract class WordControllerImpl<E extends Word, S extends WordServiceIm
     @Override
     @GetMapping("/ids/{id}")
     public ResponseEntity<?> getOne(@PathVariable Long id) {
-        WordDto wordDto = modelMapper.map(service.findById(id), WordDto.class);
+        WordDto wordDto = WordDto.convertToDto(service.findById(id));
         return new ResponseEntity<>(wordDto, HttpStatus.OK);
     }
 
     @Override
     @GetMapping("/refs/{idWordRef}")
     public ResponseEntity<?> getOneByWordRef(@PathVariable Long idWordRef) {
-        WordDto wordDto = modelMapper.map(service.findByWordReference(idWordRef), WordDto.class);
+        WordDto wordDto = WordDto.convertToDto(service.findByWordReference(idWordRef));
         return new ResponseEntity<>(wordDto,
                 HttpStatus.OK);
     }
@@ -54,7 +50,7 @@ public abstract class WordControllerImpl<E extends Word, S extends WordServiceIm
     public ResponseEntity<?> getAllById(@RequestBody List<Long> ids) {
         List<WordDto> list = service.findAllById(ids)
                 .stream()
-                .map(word -> modelMapper.map(word, WordDto.class))
+                .map(WordDto::convertToDto)
                 .toList();
 
         return new ResponseEntity<>(list, HttpStatus.OK);
@@ -65,7 +61,7 @@ public abstract class WordControllerImpl<E extends Word, S extends WordServiceIm
     public ResponseEntity<?> getAllByIdWordReferences(@RequestBody List<Long> idsWordReferences) {
         List<WordDto> list = service.findByWordReferenceIn(idsWordReferences)
                 .stream()
-                .map(word -> modelMapper.map(word, WordDto.class))
+                .map(WordDto::convertToDto)
                 .toList();
 
         return new ResponseEntity<>(list, HttpStatus.OK);
@@ -76,7 +72,7 @@ public abstract class WordControllerImpl<E extends Word, S extends WordServiceIm
     public ResponseEntity<?> getAllWordStringFilter(@PathVariable String filter) {
         List<WordDto> list = service.findByWordStringContaining(filter)
                 .stream()
-                .map(word -> modelMapper.map(word, WordDto.class))
+                .map(WordDto::convertToDto)
                 .toList();
 
         return new ResponseEntity<>(list, HttpStatus.OK);
@@ -85,14 +81,14 @@ public abstract class WordControllerImpl<E extends Word, S extends WordServiceIm
     @Override
     @PostMapping("")
     public ResponseEntity<?> save(@RequestBody E entity) {
-        WordDto wordDto = modelMapper.map(service.save(entity), WordDto.class);
+        WordDto wordDto = WordDto.convertToDto(service.save(entity));
         return new ResponseEntity<>(wordDto, HttpStatus.OK);
     }
 
     @Override
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable Long id, @RequestBody E entity) {
-        WordDto wordDto = modelMapper.map(service.update(id, entity), WordDto.class);
+        WordDto wordDto = WordDto.convertToDto(service.update(id, entity));
         return new ResponseEntity<>(wordDto, HttpStatus.OK);
     }
 
